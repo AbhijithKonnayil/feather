2af3be461cfc1beb906af1e2431249b95ce97fa4
Abhijith K <iamabhijith.k@gmail.com>
2025-09-03 21:47:30 +0530

    init`feather_cli`

---
+++ packages/feather_cli/lib/feather_cli.dart
@@ -0,0 +1,10 @@
+/// feather_cli, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate feather_cli
+/// dart pub global activate feather_cli
+///
+/// # see usage
+/// feather_cli --help
+/// ```
+library;

73fc61bbeb1009c072f68a8758eb54759135e22b
Abhijith K <iamabhijith.k@gmail.com>
2025-09-08 17:44:02 +0530

    add json_combination_builder

--- packages/feather_generator/lib/feather_generator.dart
+++ packages/feather_generator/lib/feather_generator.dart
@@ -1,6 +1,8 @@
 import 'package:build/build.dart' show Builder, BuilderOptions;
 import 'package:feather_generator/src/builders/json_builder.dart'
     show JsonBuilder;
+import 'package:feather_generator/src/builders/json_combination_builder.dart'
+    show JsonCombinationBuilder;
 import 'package:feather_generator/src/generators/widget_meta_json_generator.dart'
     show WidgetMetaJsonGenerator;
 
@@ -12,3 +14,6 @@ Builder widgetMetaJsonBuilder(BuilderOptions options) => JsonBuilder(
   },
   generatedExtension: '.feather_widget_meta.json',
 );
+
+Builder widgetJsonCombinationBuilder(BuilderOptions options) =>
+    JsonCombinationBuilder();
---
+++ packages/feather_generator/lib/src/builders/json_combination_builder.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:build/build.dart';
+import 'package:glob/glob.dart';
+
+class JsonCombinationBuilder extends Builder {
+  @override
+  Future<void> build(BuildStep buildStep) async {
+    final glob = Glob('**.feather_widget_meta.json');
+
+    // Collect and flatten into a single list
+    final allMaps = <Map<String, dynamic>>[];
+
+    await for (final asset in buildStep.findAssets(glob)) {
+      final content = await buildStep.readAsString(asset);
+      final parsed = (jsonDecode(content) as List).cast<Map<String, dynamic>>();
+      allMaps.addAll(parsed);
+      print("allMaps $allMaps");
+    }
+
+    // Encode once, at the end
+    final jsonString = jsonEncode(allMaps);
+
+    final assetId = AssetId('feather_ui', 'lib/list.copy.json');
+    await buildStep.writeAsString(assetId, jsonString);
+  }
+
+  @override
+  final buildExtensions = const {
+    r'$lib$': ['list.copy.json'], // one output per package
+  };
+}

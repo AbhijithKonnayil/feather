d9c6c87c5024098ce55a4412c6a826da16e505b9
Abhijith K <iamabhijith.k@gmail.com>
2025-09-17 09:07:50 +0530

    Update package versions to 0.0.1, add repository links, and include LICENSE files across multiple packages; remove obsolete widget list.

--- packages/catalog_website/lib/features/catalog/list.dart
+++
@@ -1,7 +0,0 @@
-import 'package:catalog_website/_generated/widget_list.g.dart';
-import 'package:feather_core/feather_core.dart' show WidgetDetails;
-
-final Map<String, List<WidgetDetails>> widgetsByCategory = {
-  'Buttons': widgetList,
-  'TextFields': widgetList,
-};
